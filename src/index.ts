bf1ecab88420aeef1a822b4bee12aa34ae019b20
smithjke <smithjke@gmail.com>
2022-12-09 20:09:41 +0300

    fix: change exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
+export * from './api';
 export * as Auth from './auth';
 export * as Page from './page';
 export * as Session from './session';
 export * as User from './user';
-export * from './api';
